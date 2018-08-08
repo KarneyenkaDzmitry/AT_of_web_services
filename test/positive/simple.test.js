b8989d201bcf452af2b8dbca602950fb68feeb18
Dzmitry_Karneyenka <Dzmitry_Karneyenka@epam.com>
2018-08-08 13:18:41 +0300

    add different schemas for tests

--- test/positive/simple.test.js
+++ test/positive/simple.test.js
@@ -4,7 +4,7 @@ const logger = require('../../configs/logger.conf');
 const sender = require('../../utils/sender');
 const usersData = require('../../data/simple.test.json');
 const BASEURI = require('../../data/host.json').uri;
-const ajv = new require('ajv')();
+const ajv = new require('ajv')({ allErrors: true });
 const statusCode = 200;
 const statusMessage = 'OK';
 
@@ -29,14 +29,14 @@ describe(`Tests of ${BASEURI}`, () => {
             expect(response.headers['content-type']).equal(data['content-type']);
         });
 
-        it(`Verification the response body. path:[${path}]`, () => {
-            logger.info(`Checking respone's body. path:[${path}]`);
-            expect(response.body.length).equal(data['body-array-length']);
-        });
-
         it(`Verification the response body with schemas. path:[${path}]`, () => {
             logger.info(`Checking respone's body with schemas. path:[${path}]`);
-            expect(ajv.validate(require(data.schema), response.body)).equal(true);
+            const schema = require(data.schema);
+            const valid = ajv.validate(schema, response.body);
+            if (!valid) {
+                logger.debug(ajv.errors);
+            }
+            expect(valid).equal(true);
         });
 
     });
